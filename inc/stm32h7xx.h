b4ad86280b072de26d0778ef22ea85b57a16cd29
Josh <24302717+JPrier@users.noreply.github.com>
2025-04-08 04:35:49 +0000

    remove duplicate inc directory

--- inc/stm32h7xx.h
+++ inc/stm32h7xx.h
@@ -1,1 +1,16 @@
+#ifndef STM32H7XX_H
+#define STM32H7XX_H
+
+#define STM32H723xx
+
+/* Include CMSIS headers */
 #include "stm32h723xx.h"
+
+/* Define USE_HAL_DRIVER to include HAL driver */
+#define USE_HAL_DRIVER
+
+#ifdef USE_HAL_DRIVER
+  #include "stm32h7xx_hal.h"
+#endif /* USE_HAL_DRIVER */
+
+#endif /* STM32H7XX_H */
